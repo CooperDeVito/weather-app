4ee9c5b16595af4d4a4de186863d175357aa27df
Cooper DeVito <ced3rq@virginia.edu>
2021-05-17 15:54:24 -0400

    new api key

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import "./keys";
 import API_KEY from './keys';
-require('dotenv').config()
+require('dotenv').config();
 
 function App() {
   const [weather, setWeather] = useState(null);
   useEffect(() => { 
     const url = new URL("https://api.openweathermap.org/data/2.5/weather");
     url.searchParams.append("appid", API_KEY); //insert appid so they take our request
-    url.searchParams.append("zipcode", 22943); //hardcoded zipcode
+    url.searchParams.append("zipcode", 22904); //hardcoded zipcode
     fetch(url).then(resp=>  {
       return resp.json();
     }).then(obj=> {
@@ -20,7 +20,7 @@ function App() {
   return (
     <div style = {{ textAlign: 'center'}}>
       {console.log(API_KEY)}
-      {console.log("hI")}
+      
       <h1> Weather App </h1>
       {JSON.stringify(weather)}
     </div>
