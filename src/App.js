6ec8f98c04bdc85d4e556b03e2534afbc1d248db
Cooper DeVito <ced3rq@virginia.edu>
2021-05-18 12:48:01 -0400

    checking

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   useEffect(() => { 
     const url = new URL("https://api.openweathermap.org/data/2.5/weather");
     url.searchParams.append("appid", API_KEY); //insert appid so they take our request
-    url.searchParams.append("zipcode", 22904); //hardcoded zipcode
+    url.searchParams.append("zipcode", 22903); //hardcoded zipcode
     fetch(url).then(resp=>  {
       return resp.json();
     }).then(obj=> {
@@ -20,7 +20,6 @@ function App() {
   return (
     <div style = {{ textAlign: 'center'}}>
       {console.log(API_KEY)}
-      
       <h1> Weather App </h1>
       {JSON.stringify(weather)}
     </div>
